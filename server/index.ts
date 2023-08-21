e4fc3513bcea6cf75907e8e90a7eb0d17ea4ad04
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-08-21 19:22:09 +0200

    Add database loading

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,27 @@
-import { StaticFileRoute } from "vlserver";
+import { Inject, StaticFileRoute, ViewModel } from "vlserver";
 import { ManagedServer } from "./managed/server";
+import { DbContext } from "./managed/database";
 import { join } from "path";
-import { DbClient } from "vlquery";
+import { DbClient, RunContext } from "vlquery";
 
 DbClient.connectedClient = new DbClient({});
 
 DbClient.connectedClient.connect().then(async () => {
 	const app = new ManagedServer();
+	const database = new DbContext(new RunContext());
+	
+	app.createInjector = context => new Inject({
+		Context: context,
+		DbContext: database
+	});
 
 	app.use(new StaticFileRoute('/', join(process.cwd(), '..', 'page', 'built')));
 	app.use(new StaticFileRoute('/assets', join(process.cwd(), '..', 'page', 'assets')));
 
 	app.prepareRoutes();
 	app.use(new StaticFileRoute('*', join(process.cwd(), '..', 'page', 'built', 'index.html')));
+
+	ViewModel.globalFetchingContext = database;
 
 	app.start(+process.env.PORT! || {{PORT}});
 });
