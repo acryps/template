0d342de99ef32c7998c167b6baa64d040b299667
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-08-21 17:37:04 +0200

    Add database

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,18 @@
 import { StaticFileRoute } from "vlserver";
 import { ManagedServer } from "./managed/server";
 import { join } from "path";
+import { DbClient } from "vlquery";
 
-const app = new ManagedServer();
+DbClient.connectedClient = new DbClient({});
 
-app.use(new StaticFileRoute('/', join(process.cwd(), '..', 'page', 'built')));
-app.use(new StaticFileRoute('/assets', join(process.cwd(), '..', 'page', 'assets')));
+DbClient.connectedClient.connect().then(async () => {
+	const app = new ManagedServer();
+	
+	app.use(new StaticFileRoute('/', join(process.cwd(), '..', 'page', 'built')));
+	app.use(new StaticFileRoute('/assets', join(process.cwd(), '..', 'page', 'assets')));
 
-app.prepareRoutes();
-app.use(new StaticFileRoute('*', join(process.cwd(), '..', 'page', 'built', 'index.html')));
+	app.prepareRoutes();
+	app.use(new StaticFileRoute('*', join(process.cwd(), '..', 'page', 'built', 'index.html')));
 
-app.start(+process.env.PORT! || {{PORT}});
+	app.start(+process.env.PORT! || {{PORT}});
+}
